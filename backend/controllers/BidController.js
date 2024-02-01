4c2daf20354057375c5c8f038ae7899616e62b8f
Arturik <artur090806@yandex.ru>
2024-02-01 18:30:26 +0300

    4

--- backend/controllers/BidController.js
+++ backend/controllers/BidController.js
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+
 import { sendMessage } from '../utils/sendMessage.js'
 
 const prisma = new PrismaClient()
