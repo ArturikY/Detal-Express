0da42959a9527a291fea76e853b51f3d02fc8e49
Arturik <artur090806@yandex.ru>
2024-02-01 17:05:07 +0300

    first commit

---
+++ backend/controllers/BidController.js
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client'
+import { sendMessage } from '../utils/sendMessage.js'
+
+const prisma = new PrismaClient()
+
+export const create = async (req, res) => {
+	try {
+		const bid = await prisma.bid.create({
+			data: {
+				vin: req.body.vin.toUpperCase(),
+				key: req.body.key,
+				text: req.body.text,
+				author: { connect: { id: req.userId } },
+			},
+			include: {
+				author: true,
+			},
+		})
+
+		sendMessage(
+			`Имя: ${bid.author.name} \nНомер телефона: ${bid.author.phone}\nМестоположение: ${bid.author.location}\nСписок запчастей: ${bid.text}\nVIN: ${bid.vin}\nKey: ${bid.key}
+		`
+		)
+		res.json(bid)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Failed to create a bid',
+		})
+	}
+}
+
+export const getAll = async (req, res) => {
+	try {
+		const bids = await prisma.bid.findMany()
+
+		res.json(bids)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Failed to find bids',
+		})
+	}
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+import cors from 'cors'
+import express from 'express'
+import next from 'next'
+
+import {
+	BidController,
+	UserController,
+} from './backend/controllers/controllers.js'
+import { checkAuth, handleValidationErrors } from './backend/utils/utils.js'
+import {
+	BidValidations,
+	UserValidations,
+} from './backend/validations/validations.js'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+	const server = express()
+
+	server.use(express.json())
+	server.use(cors())
+
+	server.post(
+		'/api/auth/login',
+		UserValidations.loginValid,
+		handleValidationErrors,
+		UserController.login
+	)
+	server.post(
+		'/api/auth/register',
+		UserValidations.registerValid,
+		handleValidationErrors,
+		UserController.register
+	)
+	server.get('/api/auth/me', checkAuth, UserController.getMe)
+	server.patch(
+		'/api/auth/update',
+		checkAuth,
+		UserValidations.registerValid,
+		handleValidationErrors,
+		UserController.updateMe
+	)
+
+	server.get('/api/bids', BidController.getAll)
+
+	server.post(
+		'/api/bids',
+		checkAuth,
+		BidValidations.createValid,
+		handleValidationErrors,
+		BidController.create
+	)
+
+	server.all('*', (req, res) => {
+		return handle(req, res)
+	})
+
+	server.listen(3000, err => {
+		if (err) throw err
+		console.log('Ready on http://localhost:3000')
+	})
+})
